3a0abe5e9d06bf7e768b00c108c80241bc89aee9
Pam94 <pamelafernandez94@gmail.com>
2022-12-08 18:34:00 +0100

    User model and migration

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -19,6 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
     ];
@@ -38,7 +38,14 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
+
+    /**
+     * This function returns all the recipes
+     * the User has been created
+     */
+    public function recipes(){
+
+        return $this->hasMany(Recipe::class);
+    }
 }
