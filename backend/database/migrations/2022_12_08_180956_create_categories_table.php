60d020197558da485c61e23fc66c9f49d8c4faed
Pam94 <pamelafernandez94@gmail.com>
2022-12-12 00:56:35 +0100

    Category id renamed

--- backend/database/migrations/2022_12_08_180956_create_categories_table.php
+++ backend/database/migrations/2022_12_08_180956_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id('categoryId');
+            $table->id();
             $table->string('name');
             $table->binary('icon')->nullable();
             $table->timestamps();
