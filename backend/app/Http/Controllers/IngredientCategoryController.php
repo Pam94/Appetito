69167e350bc8b1874cec3a3124f179e4616c9011
Pam94 <pamelafernandez94@gmail.com>
2022-12-11 18:42:14 +0100

    Create Ingredient Category controller and resource

---
+++ backend/app/Http/Controllers/IngredientCategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\IngredientCategoryResource;
+use App\Models\IngredientCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class IngredientCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return IngredientCategoryResource::collection(IngredientCategory::latest()->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $validateNewIngredientCategory = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required'
+                ]
+            );
+
+            if ($validateNewIngredientCategory->fails()) {
+
+                return response()->json([
+                    'message' => 'Invalid new Ingredient Category parameters',
+                    'errors' => $validateNewIngredientCategory->errors()
+                ], 401);
+            }
+
+            IngredientCategory::create([
+                'name' => $request->name,
+                'icon' => $request->icon
+            ]);
+
+            return response()->json([
+                'message' => 'Ingredient Category Created Successfully'
+            ], 200);
+        } catch (\Throwable $throwable) {
+
+            return response()->json([
+                'message' => $throwable->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\IngredientCategory  $ingredientCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IngredientCategory $ingredientCategory)
+    {
+        return new IngredientCategoryResource($ingredientCategory);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\IngredientCategory  $ingredientCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IngredientCategory $ingredientCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\IngredientCategory  $ingredientCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IngredientCategory $ingredientCategory)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\IngredientController as IngredientV1;
 use App\Http\Controllers\AuthController as Auth;
+
+use App\Http\Controllers\IngredientController as IngredientV1;
+use App\Http\Controllers\IngredientCategoryController as IngredientCategoryV1;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/ingredients', IngredientV1::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    // api/v1/ingredients
+    Route::apiResource('v1/ingredients', IngredientV1::class)
+        ->only(['index', 'store', 'show']);
+
+    // api/v1/ingredientCategories
+    Route::apiResource('v1/ingredientCategories', IngredientCategoryV1::class)->only(['index', 'store', 'show']);
+});
 
 Route::post('register', [Auth::class, 'register']);
 Route::post('login', [Auth::class, 'login']);
