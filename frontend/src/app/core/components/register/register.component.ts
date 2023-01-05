32403176404427677accf8fca76bf2d2ee9ffbc2
Pam94 <pamelafernandez94@gmail.com>
2023-01-05 14:38:40 +0100

    Add formControl directives and complete register

--- frontend/src/app/core/components/register/register.component.ts
+++ frontend/src/app/core/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  errors: any = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    });
+  }
+
+  register() {
+    this.authService.register(this.registerForm.value()).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        this.errors = error.error;
+      },
+      () => {
+        this.registerForm.reset();
+        this.router.navigate(['login']);
+      }
+    );
+  }
 
 }
--- frontend/src/app/core/constants.ts
+++
@@ -1,6 +0,0 @@
-export class Constants {
-    public readonly API_ENDPOINT: string = 'http://127.0.0.1:8000/api/';
-
-    public readonly API_REGISTER: string = this.API_ENDPOINT + 'register';
-    public readonly API_LOGIN: string = this.API_ENDPOINT + 'login';
-}
