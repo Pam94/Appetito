d6a90bfd3a259cbe208418898267e4d40052ee7a
Pam94 <pamelafernandez94@gmail.com>
2023-01-05 08:44:38 +0100

    Reesctructurate shared components files

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HeaderNavComponent } from './components/header-nav/header-nav.component';
 
 
 
