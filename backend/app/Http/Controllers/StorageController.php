4b8a4ab0dddfe2f497313f6558e98ce8f1827bd7
Pam94 <pamelafernandez94@gmail.com>
2023-01-13 18:23:44 +0100

    Add Intervention Image library

--- backend/app/Http/Controllers/StorageController.php
+++ backend/app/Http/Controllers/StorageController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image as ImageIntervention;
 
 class StorageController extends Controller
 {
+
+    public function saveImage($imageFile, $imageHashName)
+    {
+        $imageFile->store('images', 'private');
+
+        $imageThumbnail = ImageIntervention::make($imageFile->getRealPath());
+
+        $imageThumbnail->resize(300, 300, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save(storage_path('app/private') . '/thumbnails/' . $imageHashName);
+    }
+
     public function getImage(string $filename)
     {
         $images_path = 'private/images/' . $filename;
+
+        if (Storage::exists($images_path)) {
+
+            $file = Storage::get($images_path);
+            return response($file, 200)
+                ->header('Content-Type', 'image/jpeg', 'image/jpg', 'image/png');
+        }
+
+        return response(404);
+    }
+
+    public function getThumbnail(string $filename)
+    {
+        $images_path = 'private/thumbnails/' . $filename;
 
         if (Storage::exists($images_path)) {
 
--- backend/config/app.php
+++ backend/config/app.php
@@ -181,10 +181,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
+
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -210,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'ImageIntervention' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
