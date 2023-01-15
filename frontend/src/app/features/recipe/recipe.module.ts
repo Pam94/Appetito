3f651754f771237b35329d020f439dc99c5227ae
Pam94 <pamelafernandez94@gmail.com>
2023-01-15 03:24:59 +0100

    Initial new recipe form layout

--- frontend/src/app/features/recipe/recipe.module.ts
+++ frontend/src/app/features/recipe/recipe.module.ts
@@ -6,12 +6,14 @@ import { RecipeRoutingModule } from './recipe-routing.module';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NewRecipeComponent } from './components/new-recipe/new-recipe.component';
 
 
 @NgModule({
   declarations: [
     RecipesComponent,
     RecipeComponent,
+    NewRecipeComponent,
   ],
   imports: [
     CommonModule,
